ee922244cfa0c021aba3dd380c31e0080c229b0c
Filipe Pina <fopina@gmail.com>
2022-03-21 18:28:07 +0000

    include only requirements.txt mentioned by the entrypoints

--- run.py
+++ run.py
@@ -10,12 +10,27 @@ This script is based on:
 `find surface/ -name requirements*.txt -type f -exec cat {} + | grep -v '^-r ' | grep -v '^#' | sort | uniq > docker/requirements_full.txt`
 
 Example of the new main function (useful for doctest)
->>> main(Path('testdata/requirements.txt'))
+>>> main(['testdata/requirements-1.txt'])
 docker[oth,other,tls]==4.1.0
 0
+>>> main(['testdata/requirements-2.txt'])
+flake8==4.0.1
+requests==2.27.1
+0
+>>> main(['testdata/requirements-1.txt', 'testdata/requirements-2.txt'])
+docker[oth,other,tls]==4.1.0
+flake8==4.0.1
+requests==2.27.1
+0
+>>> main(['testdata/requirements-3.txt'])
+requests
+0
+>>> main(['testdata/requirements-4.txt'])
+1
 """
 
-import sys
+import argparse
+import logging
 from pathlib import Path
 
 from pip._internal.req import parse_requirements
@@ -23,26 +38,40 @@ from pip._internal.req.constructors import (
     InstallRequirement,
     install_req_from_parsed_requirement,
 )
-from pip._internal.req.req_file import ParsedRequirement
+
+logger = logging.getLogger(__name__)
 
 
-def main(initial_req: Path):
-    subdir_requirements = set(
-        map(str, initial_req.parent.glob("**/*/requirements*.txt"))
-    )
-    imported_requirements = set(
-        str(initial_req.parent.joinpath(r.strip().replace("-r ", "")))
-        for r in initial_req.open("r")
-        if r.startswith("-r ")
-    )
+def build_parser():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('files', metavar='FILE', type=Path, nargs='+',
+                        help='path to requirements.txt-like file')
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help='debug verbosity')
+    return parser
 
-    diff = subdir_requirements - imported_requirements
-    if diff:
-        print(f'[Fatal] {diff} are missing from {initial_req}', file=sys.stderr)
-        return 1
+
+def main(argv=None):
+    logging.basicConfig()
+    args = build_parser().parse_args(argv)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        logger.debug('debug enabled')
+
+    imported_requirements = set(map(str, args.files))
+    
+    for initial_req in args.files:
+        with initial_req.open("r") as inf:
+            imported_requirements.update(
+                str(initial_req.parent.joinpath(r.strip()[3:]))
+                for r in inf
+                if r.startswith("-r ")
+            )
+
+    logger.debug(f'files to process: {", ".join(imported_requirements)}')
 
     ss = set()
-    for f in initial_req.parent.glob("**/requirements*.txt"):
+    for f in imported_requirements:
         ss.update(
             {
                 install_req_from_parsed_requirement(req)
@@ -59,17 +88,10 @@ def main(initial_req: Path):
             continue
 
         if not item.req.specifier:
-            print(
-                f'[Warning] {item} is not pinned to (a) specific version(s)',
-                file=sys.stderr,
-            )
+            logger.warning(f'{item} is not pinned to (a) specific version(s)')
 
         if requirements.get(item.name, item).req.specifier != item.req.specifier:
-            print(
-                f'[Fatal] {item.name} is duplicated with different versions: {item.req.specifier} vs {requirements[item.name].req.specifier}',
-                file=sys.stderr,
-            )
-
+            logger.fatal(f'{item.name} is duplicated with different versions: {item.req.specifier} vs {requirements[item.name].req.specifier}')
             return 1
 
         if item.name in requirements:
@@ -91,4 +113,4 @@ def main(initial_req: Path):
 
 
 if __name__ == '__main__':
-    sys.exit(main(Path(sys.argv[1])))
+    exit(main())
